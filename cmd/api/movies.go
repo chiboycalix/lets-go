3f664da4f64ad4af5b5ee4360d4f98b6b86258d2
chiboycalix <igwechinonso77@gmail.com>
2023-07-07 10:58:43 +0100

    json unmarshal

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -9,10 +9,10 @@ import (
 
 func (app *application) createMovieHandler(res http.ResponseWriter, req *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json: "year"`
-		Runtime int32    `json:runtime`
-		Genres  []string `json:genres`
+		Title   string       `json:"title"`
+		Year    int32        `json: "year"`
+		Runtime data.Runtime `json:runtime`
+		Genres  []string     `json:genres`
 	}
 
 	err := app.readJson(res, req, &input)
